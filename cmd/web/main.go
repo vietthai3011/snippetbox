12b6e438a32e6b717929812330c6483e750df212
vietthai.3011 <ngovietthai3011@gmail.com>
2025-02-19 19:37:01 +0700

    Connect database mySQL

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"log/slog"
 	"net/http"
 	"os"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 type application struct {
@@ -14,6 +17,9 @@ type application struct {
 func main() {
 
 	addr := flag.String("addr", ":4000", "HTTP network address")
+
+	dsn := flag.String("dsn", "web:123456@/snippetbox?parseTime=true", "MySQL data source name")
+
 	flag.Parse()
 
 	// NewJSONHandler - tạo 1 logHandle
@@ -21,11 +27,34 @@ func main() {
 	// HandlerOptions - mưc độ log
 	logger := slog.New(slog.NewTextHandler(os.Stdout, nil))
 
+	db, err := openDB(*dsn)
+
+	if err != nil {
+		logger.Error(err.Error())
+	}
+
+	defer db.Close()
 	app := &application{logger: logger}
 
-	logger.Info("starting server", "addr", addr)
+	logger.Info("starting server", "addr", *addr)
 
-	err := http.ListenAndServe(*addr, app.routers())
+	err = http.ListenAndServe(*addr, app.routers())
 	logger.Error(err.Error())
 	os.Exit(1)
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("mysql", dsn)
+
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.Ping()
+
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
 }
